93ee915dc8237a5fae39176b5dd651beacf5ed19
adelaunay <delaunay.alexandre@gmail.com>
2020-11-29 23:05:54 +0100

    showFormButtons

--- inc/html.class.php
+++ inc/html.class.php
@@ -5244,7 +5244,7 @@ JAVASCRIPT;
 
       // Set default class to submit
       if (!isset($options['class'])) {
-         $options['class'] = 'vsubmit';
+         $options['class'] = 'btn';
       }
       if (isset($options['confirm'])) {
          if (!empty($options['confirm'])) {
